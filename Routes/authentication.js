f9a1cd7b24571b40fd0f67780be019f2642c1a7c
ShravaniGavali <shravani.201934205@vcet.edu.in>
2022-12-22 15:41:17 +0530

    authentication

--- Routes/authentication.js
+++ Routes/authentication.js
@@ -1,29 +1,56 @@
-/* eslint-disable no-undef */
+/* eslint-disable handle-callback-err */
 const { Router } = require('express')
 const users = require('../models/users')
+const { encrypt, compare, createAccessToken } = require('../utils')
 
 const authRouter = Router()
 
 authRouter.post('/login', (req, res) => {
-  return res.status(200).json({
-    message: 'login successful'
-  })
+  return Promise.resolve()
+    .then(() => {
+      if (!(req.body.email && req.body.password)) {
+        throw Error('email,username,password not found')
+      }
+      return users.findOne({ email: req.body.email })
+    })
+    .then((data) => {
+      if (!data) {
+        throw Error('user not found')
+      }
+      return compare(req.body.password, data.password)
+    })
+
+    .then((match) => {
+      if (!match) {
+        throw Error('match not found')
+      }
+      return res.status(200).json({
+        message: 'Login successful',
+        access_token: createAccessToken(req.body.email)
+      })
+    })
 })
 
 authRouter.post('/register', (req, res) => {
   return Promise.resolve()
     .then(() => {
       if (!(req.body.email && req.body.username && req.body.password)) {
-        throw Error('email, username or password is not found')
+        throw Error('email,username,password not found')
       }
+      return encrypt(req.body.password)
+    })
+    .then((hash) => {
+      req.body.password = hash
       return users.create(req.body)
     })
     .then(data => {
       data = data.toJSON()
       delete data.password
 
+      data.access_token = createAccessToken(req.body.email)
+
       return res.status(200).json({
-        message: 'registered successful',
+        message: 'Registered Successfully',
         data: data
       })
     })
@@ -34,10 +61,15 @@ authRouter.post('/register', (req, res) => {
       })
     })
 })
-authRouter.post('/verify', (req, res) => {
+
+//   return res.status(200).json({
+//     message: 'Registered successful'
+//   })
+// })
+
+authRouter.post('/verify_email', (req, res) => {
   return res.status(200).json({
-    message: 'email verified successful'
+    message: 'Email Verified Successfully'
   })
 })
-
 module.exports = authRouter
